efc680dfec5fd7d76e59c821d1f47a54f2769772
Ethan Yu <sohbmpethan@gmail.com>
2020-12-06 15:08:16 +0900

    Add the restriction that only supports Google Chrome temporarily

--- src/index.js
+++ src/index.js
@@ -3,21 +3,37 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import HomePage from './HomePage';
 import MeetingRoom from './MeetingRoom';
+import Typography from '@material-ui/core/Typography';
 
 class App extends React.Component {
+  isChrome = () => {
+    return /Chrome/.test(navigator.userAgent) &&
+        /Google Inc/.test(navigator.vendor);
+  }
+
   render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/:url" component={MeetingRoom} />
-        </Switch>
-      </Router>
-    );
+    if (!this.isChrome()) {
+      return (
+        <Typography align="center" color="primary" variant="h2">
+          <br/><br/>Sorry, IVCS is only available for testing on Google Chrome
+          at this time.
+          <br/><br/>Our team will add support for other browsers in the future.
+        </Typography>
+      );
+    } else {
+      return (
+        <Router>
+          <Switch>
+            <Route exact path="/" component={HomePage}/>
+            <Route path="/:url" component={MeetingRoom}/>
+          </Switch>
+        </Router>
+      );
+    }
   }
 }
 
 ReactDOM.render(
-    <App />,
+    <App/>,
     document.getElementById('root'),
 );
